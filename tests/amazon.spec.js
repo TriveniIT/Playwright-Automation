2d75c0ed066daf54c7862f13f02f3839116ecb8e
spatil <supriya22patil@gmail.com>
2024-01-03 14:46:52 +0530

    changes made by Supriya added 1 line

--- tests/amazon.spec.js
+++ tests/amazon.spec.js
@@ -2,6 +2,7 @@ const{test,expect}= require('@playwright/test');
 test('Home Page', async({page})=>{
 await page.goto('https://www.amazon.in/');
 const title =await page.title();
+await page.title();
 
 
 });
